7cade92fa27d7ef2cb58d741e37254d2adb5240f
Joseph <mojoojoseph@gmail.com>
2020-02-20 15:37:06 +0200

    Intial Chapter 10 commit

---
+++ Chapter 10/Cards/Generics/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> people = new Dictionary<String, int>() {
+                ["Gambiza"] = 25,
+                ["Mary"] =22
+            };
+
+
+            foreach (KeyValuePair<string, int> person in people)
+            {
+                Console.WriteLine(person.Value.ToString());
+            }
+
+
+            Console.ReadLine();
+                
+        }
+    }
+}
