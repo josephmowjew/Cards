b857b019050f7e9ed9149736a3fbf23a97850d70
Joseph <mojoojoseph@gmail.com>
2020-02-24 11:17:20 +0200

    Added chapter 7 - Classes

---
+++ Chapter 7/Classes/Classes/Point.cs
@@ -0,0 +1,27 @@
+﻿#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+#endregion
+
+namespace Classes
+{
+    class Point
+    {
+        public int x;
+        public int y;
+
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+       public void Display()
+        {
+            Console.WriteLine($" point X is {this.x} and point Y is {this.y}");
+        }
+    }
+}
---
+++ Chapter 7/Classes/Classes/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Program
+    {
+
+        static void doWork()
+        {
+            //create a location  using two points
+            Point location = new Point(20, 10);
+
+            location.Display();
+
+            Console.ReadLine();
+        }
+
+        static void Main(string[] args)
+        {
+  
+            try
+            {
+                doWork();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
