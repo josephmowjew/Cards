ce1faaa4baf4ad3716dbb339688c9eef2718479b
Joseph <mojoojoseph@gmail.com>
2020-02-24 11:05:40 +0200

    implemented optional parameters methods for chapter 3

--- Chapter 3/DailyRate Using Optional Parameters/DailyRate/Program.cs
+++ Chapter 3/DailyRate Using Optional Parameters/DailyRate/Program.cs
@@ -13,9 +13,35 @@ namespace DailyRate
             (new Program()).run();
         }
 
-        void run()
+         void run()
         {
+			//calculate fee passing the DailyRate variable to use the default number of days
+            double fee = calculateFee(theDailyRate: 375.0);
+            Console.WriteLine($"Fee is {fee}");
+        }
+	
+		//define calculateFee method with optional parameters for the daily rate and number of days 
+        private double calculateFee(double theDailyRate = 500.0, int noOfDays = 1)
+        {
+            Console.WriteLine("calculateFee using two optional parameters");
+            return theDailyRate * noOfDays;
+        }
 
+		//define the calcate passing the daily rate as an optional parameter
+        private double calculateFee(double dailyRate = 500.0)
+        {
+            Console.WriteLine("calculateFee using one optional parameter");
+            int defaultNoOfDays = 1;
+            return dailyRate * defaultNoOfDays;
+        }
+
+		
+        private double calculateFee()
+        {
+            Console.WriteLine("calculateFee using hardcoded values");
+            double defaultDailyRate = 400.0;
+            int defaultNoOfDays = 1;
+            return defaultDailyRate * defaultNoOfDays;
         }
     }
 }
