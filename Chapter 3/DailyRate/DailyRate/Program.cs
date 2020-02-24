51395f7b7399d44e398ef0ae568a3ccffd2e433e
Joseph <mojoojoseph@gmail.com>
2020-02-24 10:57:43 +0200

    Added chapter 3

---
+++ Chapter 3/DailyRate/DailyRate/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyRate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (new Program()).run();
+
+            
+            
+        }
+
+        void run()
+        {
+            double dailyRate = readDouble("Please enter your daily rate: ");
+
+            int noOfDays = readInt("Please enter number of days: ");
+
+            writeFee(calculateFee(dailyRate,noOfDays));
+
+            Console.ReadLine();
+
+
+        }
+
+        private void writeFee(double v) => Console.WriteLine($"The consultancy fee was {v * 1.1} ");
+       
+
+        private double calculateFee(double dailyRate, int noOfDays) => dailyRate * noOfDays;
+       
+
+        private int readInt(string v)
+        {
+            Console.WriteLine(v);
+            string line = Console.ReadLine();
+
+            return int.Parse(line);
+        }
+
+        private double readDouble(string v)
+        {
+            Console.WriteLine(v);
+            string line = Console.ReadLine();
+
+            return double.Parse(line);
+        }
+    }
+}
---
+++ Chapter 3/Factorial/Factorial/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (new Program()).run();
+        }
+
+        void run()
+        {
+            Console.WriteLine("Please write a positive integer");
+
+            string inputValue = Console.ReadLine();
+            long factorialValue = CalculateFactorial(inputValue);
+            Console.WriteLine($"factory({inputValue}) is {factorialValue} ");
+
+            Console.ReadLine();
+        }
+
+        private long CalculateFactorial(string input)
+        {
+            //convert input value to integer
+
+            int inputValue = int.Parse(input);
+
+            long factorial(int dataValue)
+            {
+                if(dataValue == 1)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return dataValue * factorial(dataValue - 1);
+                }
+    
+            }
+
+            long factorialValue = factorial(inputValue);
+            return factorialValue;
+        }
+    }
+}
